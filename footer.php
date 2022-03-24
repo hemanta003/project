d3542b28989cf39dd577a28d28eab151a36721a8
hemanta <hkp003@gmail.com>
2022-03-24 18:10:54 +0530

    change footer and make current page active

--- footer.php
+++ footer.php
@@ -1,107 +1,76 @@
-  <!-- Footer -->
-  <footer
-            class="text-center text-lg-start text-white"
-            style="background-color: #1c2331"
-            >
-      
-  
-      <!-- Section: Links  -->
-      <section class="">
-        <div class="container text-center text-md-start mt-5">
-          <!-- Grid row -->
-          <div class="row mt-3">
-            <!-- Grid column -->
-            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-              <!-- Content -->
-              <h6 class="text-uppercase fw-bold">Company name</h6>
-              <hr
-                  class="mb-4 mt-0 d-inline-block mx-auto"
-                  style="width: 60px; background-color: #7c4dff; height: 2px"
-                  />
-              <p>
-                Here you can use rows and columns to organize your footer
-                content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                elit.
-              </p>
-            </div>
-            <!-- Grid column -->
-  
-            <!-- Grid column -->
-            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-              <!-- Links -->
-              <h6 class="text-uppercase fw-bold">Products</h6>
-              <hr
-                  class="mb-4 mt-0 d-inline-block mx-auto"
-                  style="width: 60px; background-color: #7c4dff; height: 2px"
-                  />
-              <p>
-                <a href="#!" class="text-white">MDBootstrap</a>
-              </p>
-              <p>
-                <a href="#!" class="text-white">MDWordPress</a>
-              </p>
-              <p>
-                <a href="#!" class="text-white">BrandFlow</a>
-              </p>
-              <p>
-                <a href="#!" class="text-white">Bootstrap Angular</a>
-              </p>
-            </div>
-            <!-- Grid column -->
-  
-            <!-- Grid column -->
-            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-              <!-- Links -->
-              <h6 class="text-uppercase fw-bold">Useful links</h6>
-              <hr
-                  class="mb-4 mt-0 d-inline-block mx-auto"
-                  style="width: 60px; background-color: #7c4dff; height: 2px"
-                  />
-              <p>
-                <a href="#!" class="text-white">Your Account</a>
-              </p>
-              <p>
-                <a href="#!" class="text-white">Become an Affiliate</a>
-              </p>
-              <p>
-                <a href="#!" class="text-white">Shipping Rates</a>
-              </p>
-              <p>
-                <a href="#!" class="text-white">Help</a>
-              </p>
-            </div>
-            <!-- Grid column -->
-  
-            <!-- Grid column -->
-            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-              <!-- Links -->
-              <h6 class="text-uppercase fw-bold">Contact</h6>
-              <hr
-                  class="mb-4 mt-0 d-inline-block mx-auto"
-                  style="width: 60px; background-color: #7c4dff; height: 2px"
-                  />
-              <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-              <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
-              <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-              <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
-            </div>
-            <!-- Grid column -->
-          </div>
-          <!-- Grid row -->
+
+
+<footer class="bg-dark text-white pt-5 pb-4">
+    <div class="footerbox text-center text-md-left">
+      <div class="row text-center text-md-left">
+        <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+          <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Company Name</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi iure quia dolor dolorem unde nemo?</p>
         </div>
-      </section>
-      <!-- Section: Links  -->
-  
-      <!-- Copyright -->
-      <div
-           class="text-center p-3"
-           style="background-color: rgba(0, 0, 0, 0.2)"
-           >
-        © 2020 Copyright:
-        <a class="text-white" href="https://mdbootstrap.com/"
-           >MDBootstrap.com</a
-          >
+
+
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+          <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Products </h5>
+          <p><a href="#" class="text-white" style="text-decoration: none;">The Providers</a></p>
+          <p><a href="#" class="text-white" style="text-decoration: none;">Creativity</a></p>
+          <p><a href="#" class="text-white" style="text-decoration: none;">Sourcefile</a></p>
+          <p><a href="#" class="text-white" style="text-decoration: none;">Bootstrap5 link</a></p>
+        </div>
+
+
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+          <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Links</h5>
+          <p><a href="#" class="text-white" style="text-decoration: none;">Facebook</a></p>
+          <p><a href="#" class="text-white" style="text-decoration: none;">Instagram</a></p>
+          <p><a href="#" class="text-white" style="text-decoration: none;">twitter</a></p>
+          <p><a href="#" class="text-white" style="text-decoration: none;">Email</a></p>
+        </div>
+        <div class="col-md-4 col-lg-2 col-xl-2 mx-auto mt-3">
+          <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Contact Us</h5>
+          <p>
+            <i class="fas fa-home mr-3"></i>Gtcsys,b205,CorporateRoad, profitaire A,Ahmedabad
+          </p>
+          <p>
+            <i class="fas fa-envelope mr-3"></i>gtcsys_help@gmail.com
+          </p>
+          <p>
+            <i class="fas fa-phone mr-3"></i>+91 9876543423
+          </p>
+          <p>
+            <i class="fas fa-print mr-3"></i>+91 654352829
+          </p>
+        </div>
+        <hr class="mb-4">
+        <div class="row align-items-center">
+          <div class="col-md-7 col-lg-8">
+            <p>Copyright @2022 All Rights Reserved By: <a href="#" style="text-decoration: none;"> <strong
+                  class="text-warning">The Providers</strong></a></p>
+          </div>
+          <div class="col-md-5 col-lg-4">
+                <div class="text-center text-md-right">
+                      <ul class="list-unstyled list-inline">
+                          <li class="list-inline-item">
+                              <a href="#" class="btn-floating btn-sm " style="font-size: 23px;"><i class="fab fa-facebook "></i></a>
+
+                          </li>
+
+                          <li class="list-inline-item">
+                            <a href="#" class="btn-floating btn-sm " style="font-size: 23px;"><i class="fab fa-instagram "></i></a>
+
+                        </li>
+                        <li class="list-inline-item">
+                          <a href="#" class="btn-floating btn-sm " style="font-size: 23px;"><i class="fab fa-twitter "></i></a>
+
+                      </li>
+                      <li class="list-inline-item">
+                        <a href="#" class="btn-floating btn-sm " style="font-size: 23px;"><i class="fab fa-youtube "></i></a>
+
+                    </li>
+                      </ul>
+                </div>
+
+          </div>
+        </div>
       </div>
-      <!-- Copyright -->
-    </footer>
-    <!-- Footer -->
+    </div>
+<?php wp_footer(); ?>
