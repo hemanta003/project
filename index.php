2b059a50b9182327437f92286364a0c375c51344
hemanta <hkp003@gmail.com>
2022-03-24 12:30:16 +0530

    starting with new theme

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php get_header(); ?>
+
+<div class="post">
+<div class="post-item col-md-3">
+
+<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/breakfast.avif" alt="bf"><h1>Breakfast</h1></a>
+</div>
+<div class="post-item col-md-3">
+<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/lunch.avif" alt="lunch"><h1>Lunch</h1></a>
+    
+</div>
+<div class="post-item col-md-3">
+    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/snack.jpg" alt="lunch"><h1>snack</h1></a>
+</div>
+<div class="post-item col-md-3"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/dinner.avif" alt="lunch"><h1>Dinner</h1></a>
+</div>
+
+</div>
+<div class="lowerpart col-md-3">
+  <h2>course</h2>
+  <div class="lowerpart-item"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.jpg" alt="logo">logo1</a></div>
+  <div class="lowerpart-item"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/logo2.jpg" alt="logo2">logo2</a></div>
+<div class="lowerpart-item"></div><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/logo3.png" alt="logo3">logo3</a></div>
+</div>
+<div class="lowerpart col-md-3">
+  <h2>News</h2>
+  <div class="lowerpart-item"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.jpg" alt="logo">logo4</a></div>
+  <div class="lowerpart-item"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/logo2.jpg" alt="logo2">logo5</a></div>
+<div class="lowerpart-item"></div><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/logo3.png" alt="logo3">logo6</a></div>
+</div>
+<div class="lowerpart col-md-3">
+  <h2>Blogs</h2>
+  <div class="lowerpart-item"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.jpg" alt="logo">logo7</a></div>
+  <div class="lowerpart-item"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/logo2.jpg" alt="logo2">logo8</a></div>
+<div class="lowerpart-item"></div><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/logo3.png" alt="logo3">logo9</a></div>
+
+</div>
+
+</div>
+</div>
+
+<?php get_footer(); ?>
+
+  
+  
+ 
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+
+</html>
