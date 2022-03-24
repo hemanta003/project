4d96fa6157ed10e2ffded1ac5f59e429e07ed293
hemanta <hkp003@gmail.com>
2022-03-24 17:30:15 +0530

    create template of contact page

---
+++ template-contact.php
@@ -0,0 +1,20 @@
+<?php
+//Template Name:contact us
+get_header();
+?>
+<div class="contact-template">
+
+
+hello  contact page under maintanance.....!!!<br>
+sorry for inconvinance.....
+
+
+
+
+</div>
+
+
+
+<?php
+get_footer();
+?>
