198673c738f0fb67dfa6ca668650253839ad8399
hemanta <hkp003@gmail.com>
2022-03-25 19:13:28 +0530

    catagoriwise post created and count of post also

--- template-home.php
+++ template-home.php
@@ -2,51 +2,55 @@
 //Template Name:Home
 
 get_header();
+$cat=get_categories(array('taxonomy'=>'category')); 
+
 ?>
 
 
-<div class="post">
-<div class="post-item col-md-3">
 
+
+<div class="post">
+
+
+<div class="post-item ">
 <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/breakfast.avif" alt="bf"><h1>Breakfast</h1></a>
 </div>
-<div class="post-item col-md-3">
+
+<div class="post-item ">
 <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/lunch.avif" alt="lunch"><h1>Lunch</h1></a>
-    
 </div>
-<div class="post-item col-md-3">
+
+<div class="post-item ">
     <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/snack.jpg" alt="lunch"><h1>snack</h1></a>
 </div>
-<div class="post-item col-md-3"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/dinner.avif" alt="lunch"><h1>Dinner</h1></a>
+
+<div class="post-item ">
+  <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/dinner.avif" alt="lunch"><h1>Dinner</h1></a>
 </div>
 
-</div>
-<div class="lowerpart col-md-3">
-  <h2>course</h2>
-  <div class="lowerpart-item"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.jpg" alt="logo">logo1</a></div>
-  <div class="lowerpart-item"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/logo2.jpg" alt="logo2">logo2</a></div>
-<div class="lowerpart-item"></div><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/logo3.png" alt="logo3">logo3</a></div>
-</div>
-<div class="lowerpart col-md-3">
-  <h2>News</h2>
-  <div class="lowerpart-item"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.jpg" alt="logo">logo4</a></div>
-  <div class="lowerpart-item"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/logo2.jpg" alt="logo2">logo5</a></div>
-<div class="lowerpart-item"></div><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/logo3.png" alt="logo3">logo6</a></div>
-</div>
-<div class="lowerpart col-md-3">
-  <h2>Blogs</h2>
-  <div class="lowerpart-item"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.jpg" alt="logo">logo7</a></div>
-  <div class="lowerpart-item"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/logo2.jpg" alt="logo2">logo8</a></div>
-<div class="lowerpart-item"></div><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/logo3.png" alt="logo3">logo9</a></div>
 
-</div>
-
-</div>
 </div>
 
 
 
 
+<div class="lowerpart ">
+  <h2>ORder NOW:</h2>
+  <?php
+  
+foreach($cat as $catvalue){
+
+?>
+  <div class="lowerpart-item">
+
+    <img src="<?php echo get_template_directory_uri(); ?>/images/logo.jpg" alt="logo">
+    <a href="<?php echo get_category_link($catvalue->term_id); ?>"><?php echo $catvalue->name;  ?>
+  (<?php echo $catvalue->count;  ?>)</a>
+
+</div>
+  <?php } ?> 
+
+</div>
 
 
 
