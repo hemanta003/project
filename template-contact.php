3c85ab4b05adfa06a7858c26afc01fbec2955a42
hemanta <hkp003@gmail.com>
2022-03-25 12:48:55 +0530

    comment section added in the  blog page

--- template-contact.php
+++ template-contact.php
@@ -1,15 +1,13 @@
 <?php
 //Template Name:contact us
 get_header();
+the_post();
 ?>
 <div class="contact-template">
 
 
 hello  contact page under maintanance.....!!!<br>
 sorry for inconvinance.....
-
-
-
 
 </div>
 
