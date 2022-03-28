33a21479c34f5573f17f6559e039dc685a7a7b5b
hemanta <hkp003@gmail.com>
2022-03-28 11:00:32 +0530

    add short description in page using excerpt function

--- functions.php
+++ functions.php
@@ -12,5 +12,6 @@ add_theme_support('custom-header');
 
 //     )
 // );
- add_theme_support('custom-background'); 
+ add_theme_support('custom-background');
+ add_post_type_support('page','excerpt'); 
  ?>
--- page.php
+++ page.php
@@ -4,15 +4,20 @@ the_post();
 ?>
 
 <div class="about-content">
+
 <h2><?php the_title(); ?>:</h2>
+
 <?php the_post_thumbnail('thumbnail'); ?>
+<?php the_excerpt(); ?>
+
+<?php $imagepath=wp_get_attachment_image_src(get_post_thumbnail_id(),'thumbnail');
+ ?>
+ 
+<img src="<?php echo $imagepath[0] ?>" alt="">
 <?php
 echo get_the_content();
 
 ?>
-<?php $imagepath=wp_get_attachment_image_src(get_post_thumbnail_id(),'thumbnail');
- ?>
-<img src="<?php echo $imagepath[0] ?>" alt="">
 </div>
 
 <?php
